c5fcb9c83f44a1158f5fc6d3b335ec51aca12ebf
dneumann <dneumann@mhighpoint.com>
2026-07-22 10:51:00 -0700

    Ordering pragma

--- src/plugnim.nim
+++ src/plugnim.nim
@@ -1,17 +1,31 @@
-import std/[macros, macrocache, strformat, strutils]
+import std/[macros, macrocache, strformat, strutils, algorithm]
 
 const
   PluginFunctions = CacheTable"PluginFunctions"
 
-func pluginFunctionKey(pluginId: string, functionName: string): string =
-  &"{functionName}|{pluginId}"
+func pluginFunctionKey(pluginId: string, functionName: string, order: int): string =
+  &"{functionName}|{pluginId}|{order}"
 
 func pluginFunctionIdentifier(pluginId: string, functionName: string): string =
   &"{functionName}{pluginId}"
 
-func extractFunctionNameAndPluginId(k: string): tuple[functionName, pluginId: string] =
+func extractFunctionNamePluginIdAndOrder(k: string): tuple[functionName, pluginId: string, order: int] =
   let splits = k.split {'|'}
-  result = (splits[0], splits[1])
+  result = (splits[0], splits[1], parseInt(splits[2]))
+
+proc extractOrder(procDef: NimNode): int =
+  result = 0
+  let pragmas = procDef.pragma
+  if pragmas.kind != nnkPragma:
+    return
+  for index in 0 ..< pragmas.len:
+    let p = pragmas[index]
+    if p.kind == nnkExprColonExpr and p[0].eqIdent("order"):
+      result = parseInt(p[1].repr)
+      pragmas.del(index)
+      break
+  if pragmas.len == 0:
+    procDef.pragma = newEmptyNode()
 
 macro plugin*(identifier: untyped, body: untyped): auto =
   expectKind(identifier, nnkIdent)
@@ -19,7 +33,8 @@ macro plugin*(identifier: untyped, body: untyped): auto =
   for stmt in body:
      expectKind(stmt, nnkProcDef)
      let functionName = stmt[0].repr
-     let key = pluginFunctionKey(identifier.repr, functionName)
+     let order = extractOrder(stmt)
+     let key = pluginFunctionKey(identifier.repr, functionName, order)
      PluginFunctions[key] = stmt
      var stmt2 = stmt
      stmt2[0] = ident(pluginFunctionIdentifier(identifier.repr, functionName))
@@ -27,9 +42,9 @@ macro plugin*(identifier: untyped, body: untyped): auto =
 
 macro generatePluginFunctionCalls*(functionName: untyped): auto =
   expectKind(functionName, nnkIdent)
-  var stmts = nnkStmtList.newTree()
+  var calls: seq[tuple[order: int, call: NimNode]]
   for k, v in PluginFunctions:
-    let (fnName, pluginId) = extractFunctionNameAndPluginId(k)
+    let (fnName, pluginId, order) = extractFunctionNamePluginIdAndOrder(k)
     if functionName.repr != fnName:
       continue
     let formalParams = v[3]
@@ -39,20 +54,35 @@ macro generatePluginFunctionCalls*(functionName: untyped): auto =
     for index in 1 ..< formalParams.len:
       let param = formalParams[index]
       call.add(ident(param[0].repr))
-    stmts.add(call)
-  result = stmts
+    calls.add((order, call))
+  result = nnkStmtList.newTree()
+  for entry in calls.sortedByIt(it.order):
+    result.add(entry.call)
 
 when isMainModule:
   expandMacros:
     plugin ABC:
-      proc load =
-        discard
+      proc load {.order: 10.} =
+        echo "ONE"
       proc chode =
         echo "CHODE"
       proc update(dt: float) =
         discard
+        
+    plugin DEF:
+      proc load {.order: 9.} =
+        echo "TWO"
+      proc update(dt: float) =
+        discard
+        
+    plugin GHI:
+      proc load(msg: string) {.order: 8.} =
+        echo "THREE"
+      proc update(dt: float) =
+        discard
 
     let dt = 0.0016
+    let msg = "Banana"
     generatePluginFunctionCalls(update)
     generatePluginFunctionCalls(load)
     generatePluginFunctionCalls(chode)
